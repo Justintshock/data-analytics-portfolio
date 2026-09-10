fd7e26fada0956f3f04f36eb1ce2e79cf1f344e8
Justintshock <jtshoc01@louisville.edu>
2026-09-10 16:55:59 -0400

    Create 01_sales_analysis.sql

---
+++ retail-sales-analysis/01_sales_analysis.sql
@@ -0,0 +1,9 @@
+
+
+-- Question 1:
+-- How much total sales and profit did the business generate?
+
+SELECT 
+ ROUND ( SUM ("Sales"),2) AS total_sales,
+ ROUND ( SUM ("Profit"),2) AS total_profit 
+FROM "ecommerce_sales_data .csv"
