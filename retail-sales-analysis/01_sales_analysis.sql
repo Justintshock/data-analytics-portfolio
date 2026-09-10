e59b80ac43a8e93d3474a31f86d9348b50b76d81
Justintshock <jtshoc01@louisville.edu>
2026-09-10 17:03:51 -0400

    Add SQL query for sales by product category

--- retail-sales-analysis/01_sales_analysis.sql
+++ retail-sales-analysis/01_sales_analysis.sql
@@ -8,3 +8,16 @@ SELECT
  ROUND ( SUM ("Profit"),2) AS total_profit 
 FROM "ecommerce_sales_data .csv"
 
+
+-- Question 2:
+-- Which product categories generate the most sales and profit?
+
+
+SELECT
+    "Category",
+    ROUND(SUM("Sales"), 2) AS total_sales,
+    ROUND(SUM("Profit"), 2) AS total_profit
+FROM "ecommerce_sales_data .csv"
+  GROUP BY "Category"
+    ORDER BY total_sales DESC
+
