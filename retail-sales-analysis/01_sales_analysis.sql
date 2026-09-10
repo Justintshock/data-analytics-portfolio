83449231ac43f8dbdffcec5e085a31c5b6a3b60a
Justintshock <jtshoc01@louisville.edu>
2026-09-10 17:07:00 -0400

    Add sales and profit analysis by region
    Added a query to analyze sales and profit by region.

--- retail-sales-analysis/01_sales_analysis.sql
+++ retail-sales-analysis/01_sales_analysis.sql
@@ -21,3 +21,14 @@ FROM "ecommerce_sales_data .csv"
   GROUP BY "Category"
     ORDER BY total_sales DESC
 
+-- Question 3:
+-- Which regions generate the most sales and profit?
+
+SELECT
+    "Region",
+    ROUND(SUM("Sales"), 2) AS total_sales,
+    ROUND(SUM("Profit"), 2) AS total_profit
+FROM "ecommerce_sales_data .csv"
+  GROUP BY "Region"
+    ORDER BY total_sales DESC
+
